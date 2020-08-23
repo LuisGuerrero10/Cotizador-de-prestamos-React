a8d82c2bdc30c3cbb8ac68168ae52fed22cb7b11
Luis Guerrero <luismarlayjose10@gmail.com>
2020-08-23 16:30:54 -0500

    Cotizador de Prestamos

---
+++ src/componentes/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// function Header(props) {
+//     console.log(props);
+
+//     return(
+//         <h1>{props.titulo}</h1>
+//     )
+// }
+
+const  Header = ({titulo}) => (
+    <h1>{titulo}</h1>
+  )
+ 
+export default Header;
